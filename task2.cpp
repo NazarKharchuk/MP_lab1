e4b0868fabb9a3054fcd503d50c9b880ede28047
Nazar <nazariykharchuk@gmail.com>
2022-02-18 18:21:55 +0200

    added list of pages

--- task2.cpp
+++ task2.cpp
@@ -6,17 +6,19 @@ using namespace std;
 int main() {
 	cout << "\tTask 2)\n";
 
-	string word, correctWord, tempStr, tempPages;
+	string word, correctWord, tempStr, tempPages, strToStr;
 	int i, j, countStop = 22, tempInt, N = 25;
 	string stopWords[] = { "the", "for", "at", "a", "in", "is", "on", "are", "am", "do", "did", "to", "so", "of", "or", "not", "and", "was", "no", "but", "has", "us" };
 	int countWords = 0;
 	string* allWords = new string[countWords];
 	int* allCount = new int[countWords];
+	int* allLastPages = new int[countWords];
 	string* allPages = new string[countWords];
 	string* tempAllWords;
 	int* tempAllCount;
 	string* tempAllPages;
-	int lines = 1, pages;
+	int* tempLastPages;
+	int lines = 1, pages, tempPage;
 
 	ifstream input;
 	input.open("input.txt");
@@ -28,7 +30,7 @@ newLine:
 		input.get();
 		goto newLine;
 	}
-	cout << lines << endl;
+	//cout << lines << endl;
 
 	pages = lines / 45 + 1;
 	if (lines % 45 == 0) pages = lines / 45;
@@ -64,13 +66,26 @@ mbStopWord:
 		goto mbStopWord;
 	}
 
-	cout << correctWord << ";\t" << pages << endl;
+	//cout << correctWord << ";\t" << pages << endl;
 
 	i = 0;
 countPoint:
 	if (i < countWords) {
 		if (allWords[i] == correctWord) {
 			allCount[i]++;
+			if (allCount[i] <= 100 && allLastPages[i] != pages) {
+				strToStr = "";
+				tempPage = pages;
+				toStr:
+					if (tempPage != 0) {
+						strToStr = char(tempPage % 10 + 48) + strToStr;
+						tempPage = tempPage / 10;
+						goto toStr;
+					}
+				allPages[i] += ", " + strToStr ;
+				//cout << "!!!\t" <<strToStr << endl;
+				allLastPages[i] = pages;
+			}
 			goto newLine;
 		}
 		i++;
@@ -79,26 +94,46 @@ countPoint:
 
 	tempAllWords = new string[countWords + 1];
 	tempAllCount = new int[countWords + 1];
+	tempAllPages = new string[countWords + 1];
+	tempLastPages = new int[countWords + 1];
 	i = 0;
 copyMass:
 	if (i < countWords) {
 		tempAllWords[i] = allWords[i];
 		tempAllCount[i] = allCount[i];
+		tempAllPages[i] = allPages[i];
+		tempLastPages[i] = allLastPages[i];
 		i++;
 		goto copyMass;
 	}
 	tempAllWords[countWords] = correctWord;
 	tempAllCount[countWords] = 1;
+	tempAllPages[countWords] = "";
+	strToStr = "";
+	tempPage = pages;
+	toStr2:
+		if (tempPage != 0) {
+			strToStr = char(tempPage % 10 + 48) + strToStr;
+			tempPage = tempPage / 10;
+			goto toStr2;
+		}
+	tempAllPages[countWords] += strToStr;
+	//cout << "!\t" << tempAllPages[i] << endl;
+	tempLastPages[countWords] = pages;
 
 	countWords++;
 	delete[] allWords;
 	delete[] allCount;
+	delete[] allLastPages;
+	delete[] allPages;
 	allWords = tempAllWords;
 	allCount = tempAllCount;
+	allPages = tempAllPages;
+	allLastPages = tempLastPages;
 	goto newLine;
 
 point:
-	i = 0;
+/*	i = 0;
 sort1:
 	if (i < countWords)
 	{
@@ -121,13 +156,15 @@ sort1:
 		}
 		i++;
 		goto sort1;
-	}
+	}*/
 
 	i = 0;
 outPoint:
 	if (i < countWords && i < N) {
 		cout << allWords[i] << " - ";
-		cout << allCount[i] << endl;
+		cout << allCount[i] << " - ";
+		cout << allLastPages[i] << " - ";
+		cout << allPages[i] << endl;
 		i++;
 		goto outPoint;
 	}
@@ -137,6 +174,7 @@ outPoint:
 	delete[] allWords;
 	delete[] allCount;
 	delete[] allPages;
+	delete[] allLastPages;
 
 	return 0;
 }
